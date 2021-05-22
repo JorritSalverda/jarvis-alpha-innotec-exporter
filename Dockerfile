f13d3fdd4addd0f12e00a4d38234bc5eae96868a
Jorrit Salverda <jorrit.salverda@gmail.com>
2021-05-22 19:48:13 +0200

    build with cargo chef

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,34 @@
-FROM rust:1.52 as builder
+FROM rust:1.52 as base
 WORKDIR /app
 RUN apt-get update && apt-get install -y --no-install-recommends musl-tools
 RUN rustup target add x86_64-unknown-linux-musl
+
+FROM base as planner
+# We only pay the installation cost once, 
+# it will be cached from the second build onwards
+# To ensure a reproducible build consider pinning 
+# the cargo-chef version with `--version X.X.X`
+RUN cargo install cargo-chef --version 0.1.20
 COPY . .
-# add following 2 lines after initial build to speed up next builds
-COPY --from=jsalverda/jarvis-alpha-innotec-exporter:dlc-builder /app/target target
-COPY --from=jsalverda/jarvis-alpha-innotec-exporter:dlc-builder /usr/local/cargo /usr/local/cargo
+RUN cargo chef prepare  --recipe-path recipe.json
+
+FROM base as cacher
+RUN cargo install cargo-chef --version 0.1.20
+COPY --from=planner /app/recipe.json recipe.json
+# # add following 2 lines after initial build to speed up next builds
+# COPY --from=jsalverda/jarvis-alpha-innotec-exporter:dlc-cacher /app/target target
+# COPY --from=jsalverda/jarvis-alpha-innotec-exporter:dlc-cacher /usr/local/cargo /usr/local/cargo
+RUN cargo chef cook --release --target x86_64-unknown-linux-musl --recipe-path recipe.json
+
+FROM base
+COPY --from=cacher /app/target target
+COPY --from=cacher /usr/local/cargo /usr/local/cargo
+COPY . .
 RUN cargo test --release --target x86_64-unknown-linux-musl
 RUN cargo build --release --target x86_64-unknown-linux-musl
 
-FROM scratch AS runtime
+FROM scratch
 USER 1000
-COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
-COPY --from=builder /app/target/x86_64-unknown-linux-musl/release/jarvis-alpha-innotec-exporter .
+COPY --from=alpine:latest /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
+COPY --from=3 /app/target/x86_64-unknown-linux-musl/release/jarvis-alpha-innotec-exporter .
 ENTRYPOINT ["./jarvis-alpha-innotec-exporter"]
--- dlc.sh
+++ dlc.sh
@@ -2,16 +2,44 @@
 set -e
 
 docker build \
-	--target builder \
-	--tag jsalverda/jarvis-alpha-innotec-exporter:dlc-builder \
-	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-builder \
+	--target base \
+	--tag jsalverda/jarvis-alpha-innotec-exporter:dlc-base \
+	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-base \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
-docker push jsalverda/jarvis-alpha-innotec-exporter:dlc-builder
+docker push jsalverda/jarvis-alpha-innotec-exporter:dlc-base
 
 docker build \
-	--target runtime \
+	--target planner \
+	--tag jsalverda/jarvis-alpha-innotec-exporter:dlc-planner \
+  --cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-base \
+	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-planner \
+	--build-arg BUILDKIT_INLINE_CACHE=1 .
+docker push jsalverda/jarvis-alpha-innotec-exporter:dlc-planner
+
+docker build \
+	--target cacher \
+	--tag jsalverda/jarvis-alpha-innotec-exporter:dlc-cacher \
+  --cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-base \
+  --cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-planner \
+	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-cacher \
+	--build-arg BUILDKIT_INLINE_CACHE=1 .
+docker push jsalverda/jarvis-alpha-innotec-exporter:dlc-cacher
+
+# docker build \
+# 	--target builder \
+# 	--tag jsalverda/jarvis-alpha-innotec-exporter:dlc-builder \
+#   --cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-base \
+#   --cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-planner \
+# 	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-cacher \
+# 	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-builder \
+# 	--build-arg BUILDKIT_INLINE_CACHE=1 .
+# docker push jsalverda/jarvis-alpha-innotec-exporter:dlc-builder
+
+docker build \
 	--tag jsalverda/jarvis-alpha-innotec-exporter:dlc \
-	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-builder \
+  --cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-base \
+  --cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-planner \
+	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc-cacher \
 	--cache-from jsalverda/jarvis-alpha-innotec-exporter:dlc \
 	--build-arg BUILDKIT_INLINE_CACHE=1 .
 docker push jsalverda/jarvis-alpha-innotec-exporter:dlc
